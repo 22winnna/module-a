7bb4cd387c554337bacf69f85b66516feba59d4e
22winnna <49483246+22winnna@users.noreply.github.com>
2019-04-10 12:44:13 -0400

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# Naomi Winn
+#04/10/19
+
+######################
+#    Hello World     #
+#                    #
+#  Our first Python  #
+#     Application    #
+######################
+
+#print hello world on to the screen
+print("Hello, world")
