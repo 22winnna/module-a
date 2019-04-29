afc0c98825be26ccc8fc900efb1127e5f18edfdf
22winnna <49483246+22winnna@users.noreply.github.com>
2019-04-29 11:51:42 -0400

    Added moda_challenge.py

---
+++ moda_challenge.py
@@ -0,0 +1,27 @@
+print("A.")
+for i in range (10):
+   print("Hello, Python")
+
+print("B.")
+counter = 0
+for i in range (10):
+    print(counter)
+    counter = counter + 1
+
+print("C.")
+counter = 1
+for i in range (10):
+    print(counter)
+    counter = counter + 1
+
+print("D.")
+counter = 2
+for i in range (10):
+    print(counter)
+    counter = counter + 2
+
+print("E.")
+for i in range (1,11):
+    print(i * i)
+   
+
